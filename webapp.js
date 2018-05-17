f3f88118840d73f7055bd0bdc5cc1d0ad70c662b
Irving Angulo <irvang.manage@gmail.com>
2018-05-16 20:32:28 -0400

    using splice

--- webapp.js
+++ webapp.js
@@ -7,7 +7,7 @@ const notes = [
 	'http is a protocol',
 	'http requests have a url, method, header, and body',
 	'this is cool',
-	'that is cooool'
+	'that is hot'
 ];
 
 //assets is 'replaced' with public; assets will point to public
@@ -17,7 +17,7 @@ app.use('/assets', express.static('public'));
 // app.get('/', (req, res) => res.send('Hello Worldsssss!'));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan('tiny'));
 // app.use('/', express.static('views'));
@@ -26,22 +26,37 @@ app.get('/', (req, res) => {
 	res.status(200).render('notes', { notes: notes });
 });
 
-app.post('/notes', (req, res)=> {
+app.post('/notes', (req, res) => {
 	console.log(req.body.note);
 	notes.push(req.body.note);
 
 	// redirects to '/'; browser will do all process as visting root would do
-	res.redirect('/'); 
+	res.redirect('/');
 })
 
-//TEST with: curl -v -X "DELETE" http://localhost:3000/notes/1
-app.delete('/notes/:id', (req, res) => {
-	let {id} = req.params;
-	if (req.params.id >= 0 && req.params.id < notes.length) {
-		notes.splice(req.params.id, 1);
-		res.send(`\nnote ${req.params.id} deleted \n\n`);
+// curl -v -X PUT -d note="Updated note text." http://localhost:3000/notes/1
+app.put('/notes/:id', function (req, res) {
+	let { id } = req.params;
+	let { note } = req.body;
+
+	if (id < 0 || id >= notes.length) {
+		res.status(404).send(`\n There is no note at position ${id} \n\n`);
 	} else {
-		res.status(404).send(`\n No note not found at index ${req.params.id}. \n\n `);
+		notes.splice(id, 1, note);// also like this notes[id] = note;
+		res.status(200).send(`\nNew note saved at position ${id} \n\n`);
+	}
+
+});
+
+//TEST with: curl -v -X "DELETE" http://localhost:3000/notes/1
+app.delete('/notes/:id', function (req, res) {
+	let id = req.params.id;
+
+	if (id >= 0 && id < notes.length) {
+		notes.splice(id, 1);
+		res.send(`\nnote ${id} deleted! \n\n`);
+	} else {
+		res.status(404).send(`\n No note not found at index ${id}. \n\n `);
 	}
 });
 
