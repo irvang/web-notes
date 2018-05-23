dd687f0150e128c98a0200da34265774a6c2381b
Irving Angulo <irvang.manage@gmail.com>
2018-05-22 20:53:26 -0400

    Added delete and put.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-console.log('js code is running');
+function deleteNote(id) {
+	let ajaxRequest = {
+		url: '/notes/' + id,
+		method: 'delete',
+		success: window.location.reload()
+		// success: function () {
+		// 	window.location.reload();
+		// }
+	}
+
+	$.ajax(ajaxRequest);
+	
+}
--- webapp.js
+++ webapp.js
@@ -12,24 +12,21 @@ let notes = [
 
 //assets is 'replaced' with public; assets will point to public
 app.use('/assets', express.static('public'));
-// app.use('/css', express.static('css'));
 
-// app.get('/', (req, res) => res.send('Hello Worldsssss!'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 // app.use('/', express.static('views'));
 
 app.get('/', (req, res) => {
-	// console.log(notes)
-	res.status(200).render('notes', { notes: notes });
+	// console.log('called again');
+	res.status(200).render('notes', { notesInEjs: notes});
 });
 
 app.post('/notes', (req, res) => {
-	// console.log(req.body.note);
 	notes.push(req.body.note);
  
 	// redirects to '/'; browser will do all process as visting root would do
@@ -45,29 +42,24 @@ app.put('/notes/:id', function (req, res) {
 		res.status(404).send(`\n There is no note at position ${id} \n\n`);
 	} else {
 		notes.splice(id, 1, note);// also like this notes[id] = note;
-		res.redirect('/');
+		res.send('note deleted');
+		// res.redirect('/');
 		// res.status(200).send(`\nNew note saved at position ${id} \n\n`);
 	} 
-});
+}); 
 
 app.put('/all-notes', function (req, res) {
-	// let { id } = req.params;
-	// let { note } = req.body;
 
 	if(req.body.notes) {
 		notes = ( req.body.notes);
-		// console.log( notes);
-		// res.send('success');
-		res.status(200).redirect('/');
 	} else {
 		res.status(404).send('No notes to update');
 	}
-	
 });
 
 //TEST with: curl -v -X "DELETE" http://localhost:3000/notes/1
 app.delete('/notes/:id', function (req, res) {
-	let id = req.params.id;
+	let {id} = req.params;
 
 	if (id >= 0 && id < notes.length) {
 		notes.splice(id, 1);
